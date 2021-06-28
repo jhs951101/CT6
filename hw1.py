eefef367659c49af93932280808d20093a732492
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:21:03 +0900

    m

---
+++ hw1.py
@@ -0,0 +1,8 @@
+﻿print('1~100까지에서 3과 8의 공배수 : ', end='')
+n = 1
+
+while n <= 100:
+    if n%3 == 0 and n%8 == 0:
+        print(str(n) + '   ', end='')
+
+    n += 1
---
+++ hw2.py
@@ -0,0 +1,17 @@
+﻿password = 'lovelive'
+n = 1
+
+while True:
+    value = input('비밀번호 : ')
+
+    if value == password:
+        print('로그인 되었습니다!')
+        break
+    else:
+        if n >= 3:
+            print('로그인 실패! 횟수 초과!')
+            break
+        else:
+            print('비밀번호를 다시 입력하세요.')
+
+    n += 1
